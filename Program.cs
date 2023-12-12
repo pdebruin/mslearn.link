5db84a13661cf32c2c7f6af7d6dbfce0fd99726b
Pieter de Bruin <pdebruin@hotmail.com>
2023-12-12 18:58:34 +0100

    modalities +  appinsights

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using mslearnwasm;
+using BlazorApplicationInsights;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorApplicationInsights(config =>
+{
+    config.ConnectionString = "InstrumentationKey=a07bf12c-69ad-458c-8c4b-23648c384c81;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/";
+    config.InstrumentationKey = "a07bf12c-69ad-458c-8c4b-23648c384c81";
+});
 
 await builder.Build().RunAsync();
